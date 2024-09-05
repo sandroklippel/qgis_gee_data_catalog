3331ee91978e05a24365e0c81a8d56ffeff4024d
Sandro Klippel <sandroklippel@gmail.com>
2024-09-05 14:35:11 -0300

    add path on read dataset.json file

--- read_datasets.py
+++ read_datasets.py
@@ -2,6 +2,9 @@
 """
 
 import json
+import os.path
 
-with open("datasets.json", "r") as file:
+datasets = os.path.join(os.path.dirname(__file__), "datasets.json")
+
+with open(datasets, "r") as file:
     GEE_DATASETS = json.load(file)
